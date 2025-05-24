61c8bc1404cbb47749b53ab98366cce6893d86cd
DevMasterAryan <gunjackaryan@gmail.com>
2025-05-24 18:31:09 +0530

    GREEN: Handle newlines and custom delimiters support

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,12 +1,27 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    parse_number(numbers)
+
+    delimiter, number_string = parse_delimiter_and_numbers(numbers)
+    number_array = split_numbers(number_string, delimiter)
+    number_array.sum
   end
 
   private
 
-  def parse_number numbers
-    numbers.split(',').map(&:to_i).sum
+  def parse_delimiter_and_numbers(numbers)
+    if numbers.start_with?('//')
+      delimiter_line, number_string = numbers.split("\n", 2)
+      delimiter = delimiter_line[2..-1]
+      [delimiter, number_string]
+    else
+      [',', numbers]
+    end
+  end
+
+  def split_numbers(number_string, delimiter)
+    normalized_string = number_string.gsub("\n", delimiter)
+
+    normalized_string.split(delimiter).map(&:to_i)
   end
 end
