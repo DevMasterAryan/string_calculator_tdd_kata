1736f8cd8ab7f61089a5225528d9e25bced45eb7
DevMasterAryan <gunjackaryan@gmail.com>
2025-05-24 18:04:03 +0530

    REFACTOR: Extract number parsing into private method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    parse_number(numbers)
+  end
+
+  private
+
+  def parse_number numbers
     numbers.to_i
   end
 end
